7cbe9b64432b996d740baab7985901f66e3fb65d
etoolabode <etoolabode@gmail.com>
2021-11-24 10:54:05 +0100

    Hero changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import { Provider } from "react-redux";
 import { store } from "./Store/Store";
 import Myapp from "./Myapp";
@@ -8,6 +9,12 @@ function App() {
     <>
       <Provider store={store}>
         <Myapp />
+        <Helmet>
+          <script
+            src="//code.tidio.co/juavsp7xyimuanjgd7uayohnf654etwl.js"
+            async
+          ></script>
+        </Helmet>
       </Provider>
     </>
   );
